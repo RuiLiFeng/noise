49acba5617f0e7b53460d338f4f5358d6493e47a
unknown <ruili>
2020-04-02 16:25:23 +0800

    's'

--- run_generator_visual.py
+++ run_generator_visual.py
@@ -48,10 +48,8 @@ def generate_images(network_pkl, seeds, truncation_psi, data_dir=None, dataset_n
         tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars}) # [height, width]
         images = Gs.run(z, None, **Gs_kwargs) # [minibatch, height, width, channel]
         ops = tf.get_default_graph().get_operations()
-        ops = [op for op in ops if 'n_visual' in op.name]
-        print(ops)
+        ops = [op for op in ops if 'n_visual' in op.name.endswith('n_visual')]
         n_v = ops[0].outputs[0].eval()
-        print(n_v, ops[0])
         PIL.Image.fromarray(images[0], 'RGB').save(dnnlib.make_run_dir_path('seed%04d.png' % seed))
         PIL.Image.fromarray(n_v, 'RGB').save(dnnlib.make_run_dir_path('seed%04d-nv.png' % seed))
 
