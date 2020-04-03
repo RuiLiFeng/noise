e29a62a5bd17b491a798369ee5fed1e9df3178e4
unknown <ruili>
2020-04-03 18:30:07 +0800

    's'

--- run_generator_visual.py
+++ run_generator_visual.py
@@ -57,10 +57,9 @@ def generate_images(network_pkl, seeds, truncation_psi, data_dir=None, dataset_n
         # n_v_t = n_ops[30].outputs[0]
         # x_v_t = x_ops[30].outputs[0]
         # n_v, x_v = tflib.run([n_v_t, x_v_t], {'G_synthesis/dlatents_in:0': w, 'G_synthesis_1/dlatents_in:0': w})
-        images = misc.convert_to_pil_image(images[0], drange=training_set.dynamic_range)
+        images = misc.convert_to_pil_image(images[0], drange=training_set.dynamic_range).save(dnnlib.make_run_dir_path('seed%04d.png' % seed))
         if seed_idx == 0:
             print(n_v[0], x_v[0], m_v[0])
-        PIL.Image.fromarray(images, 'RGB').save(dnnlib.make_run_dir_path('seed%04d.png' % seed))
         PIL.Image.fromarray(n_v[0], 'L').save(dnnlib.make_run_dir_path('seed%04d-nv.png' % seed))
         PIL.Image.fromarray(x_v[0], 'L').save(dnnlib.make_run_dir_path('seed%04d-xv.png' % seed))
         PIL.Image.fromarray(m_v[0], 'L').save(dnnlib.make_run_dir_path('seed%04d-mv.png' % seed))
