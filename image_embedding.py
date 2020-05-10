a23ed72781eb80b4711dfd3437236d48180a5d1e
unknown <ruili>
2020-05-11 01:13:13 +0800

    's'

--- image_embedding.py
+++ image_embedding.py
@@ -95,7 +95,7 @@ def main():
     _, _, G = pretrained_networks.load_networks(args.network)
     # vgg = misc.load_pkl('/gdata2/fengrl/metrics/vgg16_zhang_perceptual.pkl')
     tf.keras.backend.set_image_data_format('channels_first')
-    vgg = tf.keras.applications.VGG16(include_top=False, input_shape=(3, 128, 128),
+    vgg = tf.keras.applications.VGG16(include_top=False, #input_shape=(3, 128, 128),
                                       weights='/gdata2/fengrl/metrics/vgg.h5',
                                       pooling=None)
 
